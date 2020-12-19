52b5530e52107597cf48ce14d20919529e9c74f9
unknown <m.jafarabdi@gmail.com>
2020-12-19 21:04:08 +0330

    add web static

--- inis-shop-bgworker/src/InstagramFeedService.js
+++ inis-shop-bgworker/src/InstagramFeedService.js
@@ -11,7 +11,14 @@ const InstagramFeedServiceModule = {};
 let FindNewShopsTimer = null;
 let BrowseShopTimer = null;
 
+let FindUpdateShopsTimer = null;
+let UpdateShopTimer = null;
+
 const ShopsToBrowse = [];
+
+const ShopsToUpdate = [];
+
+let shopUpdatePointer = 0;
 
 let shopPointer = 0;
 
@@ -27,6 +34,16 @@ InstagramFeedServiceModule.stop = () =>
     {
         clearInterval(FindNewShopsTimer);
     }
+    
+    if (FindUpdateShopsTimer)
+    {
+        clearInterval(FindUpdateShopsTimer);
+    }
+    
+    if (UpdateShopTimer)
+    {
+        clearInterval(UpdateShopTimer);
+    }
 
     logger.info('InstagramFeedService Stopped.');
 }
@@ -39,13 +56,179 @@ InstagramFeedServiceModule.start = () =>
         findNewShopsToBrowse();
     }, 5000);
 
-    
-    
-    BrowseShopTimer = setInterval(  () => {
-
+  
+    BrowseShopTimer = setInterval( () => {
         ScheduleBrowseShops();
-
     }, 10000);
+
+
+    FindUpdateShopsTimer = setInterval(() => {
+        findShopsToUpdate();
+    }, 8000);
+
+    UpdateShopTimer = setInterval( () => {
+        ScheduleUpdateShops();
+    }, 20000);
+}
+
+async function findShopsToUpdate()
+{
+    const now = new Date();
+    const oneHourAgo = new Date(now.getTime() - 60 * 60 * 1000); // 1 Hour Ago
+    const shop = await Shop.findOne({isUpdating: false, lastUpdateTimeStamp : {$lt : oneHourAgo}, disabled : {$ne : true} });
+    if (shop)
+    {
+        await Shop.updateOne({_id: shop._id }, {isUpdating: true});
+       
+        ShopsToUpdate.push({id: shop.id, end_cursor: null});
+    }
+
+}
+
+async function ScheduleUpdateShops()
+{
+    if (ShopsToUpdate && ShopsToUpdate.length > 0)
+    {
+        if (shopUpdatePointer >= ShopsToUpdate.length)
+        {
+            shopUpdatePointer = 0;
+        }
+
+        const {id, end_cursor} = ShopsToUpdate[shopUpdatePointer];
+        shopUpdatePointer++;
+
+        const new_cursor = await UpdateShop(id, end_cursor);
+        if (new_cursor)
+        {
+            ShopsToUpdate[shopUpdatePointer-1] = {id: id, end_cursor: new_cursor};
+        }else
+        {
+            ShopsToUpdate.splice(shopUpdatePointer-1, 1);
+            await Shop.updateOne({id: id}, {lastUpdateTimeStamp: new Date(), isUpdating: false});
+        }   
+    }
+}
+
+async function UpdateShop(shopId, after)
+{
+    const page_size = 40;
+
+    const lastPost = await Post.findOne().sort({postTimeStamp:-1}).exec();   
+    
+    logger.info(getFetchUrl(shopId, page_size, after));
+    const allRows =  await axios.get(getFetchUrl(shopId, page_size, after));
+    const allRowsData = JSON.parse(JSON.stringify(allRows.data.data));
+    const edges = allRowsData.user.edge_owner_to_timeline_media.edges;
+    const end_cursor =  allRowsData.user.edge_owner_to_timeline_media.page_info.end_cursor;
+    const has_next_page = allRowsData.user.edge_owner_to_timeline_media.page_info.has_next_page;
+
+    let found = false;
+
+   for (var i=0 ;i < edges.length; i++)
+   {
+       const item = edges[i];
+
+        const node = item.node;
+        if (node.__typename === 'GraphImage')
+        {
+            let post = {};
+            post.id = node.id;
+            post.images = [ 
+                {
+                    dimensions : node.dimensions,
+                    display_url : node.display_url,
+                    small_image_url : node.display_resources[0].src
+                }
+            ]
+            post.caption = node.edge_media_to_caption.edges[0] ?  node.edge_media_to_caption.edges[0].node.text : '';
+            post.likes = node.edge_media_preview_like.count;
+            post.timeStamp = new Date(node.taken_at_timestamp * 1000);
+
+            const postDoc = new Post(
+                {
+                    timeStamp : new Date(),
+                    shopId: shopId,
+                    id: post.id,
+                    type: node.__typename,
+                    caption: post.caption,
+                    imageUrl: post.images[0].display_url,
+                    imageUrlSmall : post.images[0].small_image_url,
+                    likes: post.likes,
+                    postTimeStamp : post.timeStamp,
+                }
+            );
+                
+            const postFound = await Post.findOne({id: post.id, shopId: shopId});
+             
+            if (!postFound)
+            {
+               await postDoc.save();
+            }
+            else
+            {
+               found = true;
+            }
+        }
+        else if (node.__typename === 'GraphSidecar')
+        {
+            let post = {};
+            post.id = node.id;
+            post.caption = node.edge_media_to_caption.edges[0] ?  node.edge_media_to_caption.edges[0].node.text : '';
+            post.likes = node.edge_media_preview_like.count;
+            post.timeStamp = new Date(node.taken_at_timestamp * 1000);
+          
+            
+            post.images = [];
+
+            node.edge_sidecar_to_children.edges.forEach(child => {
+                let photo = {};
+                photo.dimensions = child.node.dimensions;
+                photo.display_url = child.node.display_url;
+                photo.small_image_url = child.node.display_resources[0].src;
+                post.images.push(photo);
+            });
+
+            const postDoc = new Post(
+               {
+                   timeStamp : new Date(),
+                   shopId: shopId,
+                   id: post.id,
+                   type: node.__typename,
+                   caption: post.caption,
+                   imageUrl: post.images[0].display_url,
+                   imageUrlSmall: post.images[0].small_image_url,
+                   likes: post.likes,
+                   postTimeStamp: post.timeStamp,
+               }
+           );
+
+           const postFound = await Post.findOne({id: post.id, shopId: shopId});
+           if (!postFound)
+           {
+              await postDoc.save();
+              post.images.forEach( async (image, index) => {
+               const postImage = new PostImage(
+                   {
+                       timeStamp: new Date(),
+                       shopId: shopId,
+                       id: post.id,
+                       imageUrl: image.display_url,
+                       imageUrlSmall: image.small_image_url,
+                       isMainImage: (index === 0)
+                   }
+               );
+               await postImage.save();
+             });
+           }
+           else
+           {
+              found = true;
+           }
+        }
+    }
+
+   
+    return found ? null : end_cursor;
 }
 
 async function ScheduleBrowseShops ()
@@ -73,7 +256,7 @@ async function ScheduleBrowseShops ()
 }
 
 async function findNewShopsToBrowse(){   
-    const shop = await Shop.findOne({isUpdating: false, lastUpdateTimeStamp : {$eq : null} });
+    const shop = await Shop.findOne({isUpdating: false, lastUpdateTimeStamp : {$eq : null}, disabled : {$ne : true} });
     if (shop)
     {
         await Shop.updateOne({_id: shop._id }, {isUpdating: true});
