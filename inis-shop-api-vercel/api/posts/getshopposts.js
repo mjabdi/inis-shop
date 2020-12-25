f001c2788a9ffef797a687f25124d5d7f7fafb0e
mjabdi <m.jafarabdi@gmail.com>
2020-12-25 22:22:06 +0330

    add cors header

--- inis-shop-api-vercel/api/posts/getshopposts.js
+++ inis-shop-api-vercel/api/posts/getshopposts.js
@@ -28,6 +28,17 @@ export default async function handler(req, res) {
 
                     await dbConnect()
                     const posts = await Post.find({shopId: shop_id}).sort({postTimeStamp : -1}).skip(after).limit(page_size).exec()
+
+                    res.setHeader('Access-Control-Allow-Credentials', true)
+                    res.setHeader('Access-Control-Allow-Origin', '*')
+                    // another common pattern
+                    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+                    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+                    res.setHeader(
+                      'Access-Control-Allow-Headers',
+                      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+                    )
+
                     res.status(200).send({status:'OK' , count: posts.length, end_cursor: (posts.length === page_size) ? after + posts.length : null ,posts: posts})
                 }
                 catch(err)
