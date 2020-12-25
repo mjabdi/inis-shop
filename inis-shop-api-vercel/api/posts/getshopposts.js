c5ae2cd4f74a6a20e4cf84704f475e61d76aa3d7
mjabdi <m.jafarabdi@gmail.com>
2020-12-25 15:47:36 +0330

    add getposts endpoint

--- inis-shop-api-vercel/api/posts/getshopposts.js
+++ inis-shop-api-vercel/api/posts/getshopposts.js
@@ -1,11 +1,45 @@
 import checkToken from '../../utils/check-token'
+import dbConnect from '../../../utils/dbConnect'
+import Post from '../../../models/Post'
 
 export default async function handler(req, res) {
     // Get data from your database
 
     if (checkToken(req,res))
     {
-        res.status(200).json({status:'OK', posts :[]})
+        const { method } = req
+        
+        switch (method) {
+
+            case 'GET':
+                try
+                {
+                    const shop_id = req.query.shop_id
+                    let page_size = parseInt(req.query.page_size || 12)
+                    const after = parseInt(req.query.after || 0)
+                    if (page_size > 50 || page_size <= 0)
+                    {
+                        page_size = 50
+                    }
+                    if (!shop_id)
+                    {
+                        res.status(400).send({status:'FAILED' , error: 'shop_id is missing'});
+                        return;
+                    }
+
+                    await dbConnect()
+                    const posts = await Post.find({shopId: shop_id}).sort({postTimeStamp : -1}).skip(after).limit(page_size).exec()
+                    res.status(200).send({status:'OK' , count: posts.length, end_cursor: (posts.length === page_size) ? after + posts.length : null ,posts: posts})
+                }
+                catch(err)
+                {
+                    res.status(500).json({status:'FAILED', error : err.message})
+                }
+                break
+            default:
+                res.status(404).send('invalid endpoint')
+                break
+        }
     }
 }
   
--- inis-shop-api-vercel/pages/index.js
+++ inis-shop-api-vercel/pages/index.js
@@ -2,6 +2,6 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-   'Invalid Endpoint!'
+   '404 - invalid endpoint'
   )
 }
