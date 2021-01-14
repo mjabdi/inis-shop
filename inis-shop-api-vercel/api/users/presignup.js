bbfd66b957332edaaaaa4e6b5f7a6da2bed2537c
mjabdi <m.jafarabdi@gmail.com>
2021-01-14 19:40:02 +0330

    add presignup

---
+++ inis-shop-api-vercel/api/users/presignup.js
@@ -0,0 +1,44 @@
+import checkToken from '../../utils/check-token'
+import dbConnect from '../../utils/mongodb'
+import User from '../../models/User'
+import allowCors from '../../utils/allow-cors'
+import CreateRandomVerificationCode from '../../utils/verfication-code'
+import uuid from 'uuid-random'
+
+const handler = async (req, res) => {
+ 
+    if (checkToken(req, res)) {
+    const { method } = req;
+
+    switch (method) {
+      case "POST":
+        try {
+         
+          const {forename, surname, userId, password} = req.body
+          await dbConnect();
+
+          const found = await User.findOne({userId: userId})
+          if (found)
+          {
+              res.status(200).send({status:'FAILED', error: 'این شماره تلفن قبلا در سیستم ثبت شده است، اگر رمز عبور خود را فراموش کرده اید از گزینه فراموشی رمز عبور استفاده نمائید'})
+              return
+          }
+
+          const verficationCode = CreateRandomVerificationCode()
+
+          console.log(verficationCode)
+          
+          res.status(200).send({status:'OK', code: verficationCode})
+
+        } catch (err) {
+          res.status(200).json({ status: "FAILED", error: err.message });
+        }
+        break;
+      default:
+        res.status(404).send("invalid endpoint");
+        break;
+    }
+  }
+}
+
+module.exports = allowCors(handler)
